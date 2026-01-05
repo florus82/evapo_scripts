5e940f61ed61afefef59c86db6fc073726051b28
florus82 <flosanfun@hotmail.com>
2026-01-05 10:57:09 +0000

    2026-01-05

--- Guzinski/py/sharp_and_evap_Brandenburg_PARALLEL.py
+++ Guzinski/py/sharp_and_evap_Brandenburg_PARALLEL.py
@@ -17,12 +17,19 @@ from joblib import Parallel, delayed
 # paths
 lowmask_path = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/reprojected/THUENEN_GER_LST_WARP.tif'
 lowmask_bin_path = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/reprojected/THUENEN_GER_LST_WARP_BINARY.tif'
-temp_dump = '/data/Aldhani/eoagritwin/et/Sentinel3/LST/SharpEvap/Brandenburg/TileGAP/'
+temp_dump = '/data/Aldhani/eoagritwin/et/Sentinel3/LST/SharpEvap/Brandenburg/SecondShot/'
 trash_path = f'{temp_dump}trash/'
 
 path_to_slope = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/Force_Tiles/SLOPE/'
 path_to_aspect = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/Force_Tiles/ASPECT/'
 path_to_agro = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/Force_Tiles/THUENEN_2021/'
+
+path_to_dem = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/Force_Tiles/DEM/'
+path_to_lat = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/Force_Tiles/LAT/'
+path_to_lon = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/Force_Tiles/LON/'
+path_to_acq = '/data/Aldhani/eoagritwin/et/Sentinel3/LST/LST_values/Acq_time/'
+path_to_vza = '/data/Aldhani/eoagritwin/et/Sentinel3/VZA/comp/'
+path_to_vaa = '/data/Aldhani/eoagritwin/et/Sentinel3/VAA/comp/'
 
 path_to_force = '/data/Aldhani/eoagritwin/force/output/Guzinski'
 path_to_inci = '/data/Aldhani/eoagritwin/et/Auxiliary/DEM/Force_Tiles/INCIDENCE2/'
@@ -36,21 +43,22 @@ tiles_to_process = createFORCEtileLIST(list(bran['Tile_X']),
 tiles_to_process.sort()
 joblist = []
 
-# for idx, tile_to_process in enumerate(tiles_to_process):
-#     joblist.append([tile_to_process, temp_dump, path_to_slope, path_to_aspect, path_to_agro, path_to_force,
-#                     path_to_inci, path_to_lst, trash_path, '20190401', '20190410', ['maxLST'], ['S2only'], 3])
+for idx, tile_to_process in enumerate(tiles_to_process):
+    joblist.append([tile_to_process, temp_dump, path_to_slope, path_to_aspect, path_to_agro, path_to_force,
+                    path_to_inci, path_to_lst, '20190401', '20190910', ['maxLST'], ['S2only', 'allpred'], 3, False,
+                    path_to_dem, path_to_lat, path_to_lon, path_to_acq, path_to_vaa, path_to_vza])
 
 
-joblist.append([tiles_to_process[-37], temp_dump, path_to_slope, path_to_aspect, path_to_agro, path_to_force,
-                path_to_inci, path_to_lst, '20190628', '20190707', ['maxLST'], ['S2only'], 2, True])
+# joblist.append([tiles_to_process[-37], temp_dump, path_to_slope, path_to_aspect, path_to_agro, path_to_force,
+#                 path_to_inci, path_to_lst, '20190628', '20190707', ['maxLST'], ['S2only'], 2, True])
 
-joblist.append([tiles_to_process[-43], temp_dump, path_to_slope, path_to_aspect, path_to_agro, path_to_force,
-                path_to_inci, path_to_lst, '20190628', '20190707', ['maxLST'], ['S2only'], 2, True])
+# joblist.append([tiles_to_process[-43], temp_dump, path_to_slope, path_to_aspect, path_to_agro, path_to_force,
+#                 path_to_inci, path_to_lst, '20190628', '20190707', ['maxLST'], ['S2only'], 2, True])
 
 print(f'\n{len(joblist)} tiles will be processed\n')
 
 
-ncores = 2
+ncores = 26
 
 
 if __name__ == '__main__':
@@ -59,8 +67,8 @@ if __name__ == '__main__':
     print("Starting process, time:" + starttime)
     print("")
 
-    Parallel(n_jobs=ncores)(delayed(Sharp_Evap)(job[0], job[1], job[2], job[3], job[4], job[5], job[6],
-                                                job[7], job[8], job[9], job[10], job[11], job[12], job[13]) for job in joblist)
+    Parallel(n_jobs=ncores)(delayed(Sharp_Evap)(job[0], job[1], job[2], job[3], job[4], job[5], job[6], job[7], job[8], job[9], job[10],
+                                                job[11], job[12], job[13], job[14], job[15], job[16],job[17], job[18], job[19]) for job in joblist)
 
     print("")
     endtime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
